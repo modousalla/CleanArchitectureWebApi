81eb0f5d40879629e18d76b6546f76b2b6caaf80
mbom0015 <mbom0015@LR-J432-08>
2021-04-13 05:18:56 -0400

    First Commit

--- CleanArchitectureWebApi/src/Clean.Architecture.Web/SeedData.cs
+++ CleanArchitectureWebApi/src/Clean.Architecture.Web/SeedData.cs
@@ -11,29 +11,20 @@ namespace Clean.Architecture.Web
     {
         public static readonly ToDoItem ToDoItem1 = new ToDoItem
         {
-            Title = "Get Sample Working",
-            Description = "Try to get the sample to build."
+            Title = "Azure Devops - Github",
+            Description = "Intégration de l'outil Github"
         };
-        public static readonly ToDoItem ToDoItem2 = new ToDoItem
-        {
-            Title = "Review Solution",
-            Description = "Review the different projects in the solution and how they relate to one another."
-        };
-        public static readonly ToDoItem ToDoItem3 = new ToDoItem
-        {
-            Title = "Run and Review Tests",
-            Description = "Make sure all the tests run and review what they are doing."
-        };
+        
 
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var dbContext = new AppDbContext(
                 serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>(), null))
             {
-                // Look for any TODO items.
+                
                 if (dbContext.ToDoItems.Any())
                 {
-                    return;   // DB has been seeded
+                    return;  
                 }
 
                 PopulateTestData(dbContext);
@@ -49,8 +40,7 @@ namespace Clean.Architecture.Web
             }
             dbContext.SaveChanges();
             dbContext.ToDoItems.Add(ToDoItem1);
-            dbContext.ToDoItems.Add(ToDoItem2);
-            dbContext.ToDoItems.Add(ToDoItem3);
+            
 
             dbContext.SaveChanges();
         }
